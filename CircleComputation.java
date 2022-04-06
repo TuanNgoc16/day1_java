d4731783ed156373c193de93c5d97f51d2cba173
TuanNgoc16 <103101440+TuanNgoc16@users.noreply.github.com>
2022-04-07 01:53:39 +0700

    java_1

---
+++ CircleComputation.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class CircleComputation {
+
+    public static void main(String[] args) {
+
+        double radius, area, circumferece;
+
+        final double PI = 3.14159265;
+
+        radius = 1.2;
+
+        area = radius * radius * PI;
+        circumferece = 2.0 * radius * PI;
+
+        System.out.println("The Radius is");
+        System.out.println(radius);
+        System.out.println("The area is");
+        System.out.println(area);
+        System.out.println("The cỉcumference is");
+        System.out.println(circumferece);
+
+
+
+    }
+}
+
+
---
+++ PrintTest.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class PrintTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("Hello world again !");
+        System.out.println("");
+        System.out.println("Hello world");
+        System.out.println("Hello world again !");
+        System.out.println("");
+        System.out.println("Hello world");
+        System.out.println("Hello world again !");
+
+
+    }
+}
+
